2cafd25f710b09b26718031765b13c31a68328d1
Estiben Licona <39919906+estibenlicona@users.noreply.github.com>
2018-08-16 17:57:49 -0500

    18-08-18

---
+++ application/config/routes.php
@@ -0,0 +1,5 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+$route['default_controller'] = 'Home'; //Controlador Llamado Por Defecto
+$route['404_override'] = '';
+$route['translate_uri_dashes'] = FALSE;
